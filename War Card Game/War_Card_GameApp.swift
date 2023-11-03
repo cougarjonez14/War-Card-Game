ca895914583b6fbc7a8bb7ef25a08370defd4a08
stationAstral <cougarjonez@gmail.com>
2023-11-03 08:54:54 -0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by stationAstral on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
